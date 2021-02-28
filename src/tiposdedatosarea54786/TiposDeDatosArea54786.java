d80daf52bb97e56572db11cf8080fda70a05ca87
carol <carol@192.168.1.71>
2021-02-27 19:19:35 -0600

    tercer proyecto java

---
+++ src/tiposdedatosarea54786/TiposDeDatosArea54786.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatosarea54786;
+
+/**
+ *
+ * @author carol
+ */
+public class TiposDeDatosArea54786 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("AREA DEL RECTANGULO");
+        double base = 20;
+         System.out.println("La base del rectangulo es:" + base);
+        double altura = 12;
+        System.out.println("La altura del rectangulo es:" + altura);
+
+        double area = base*altura;
+        System.out.println("El area del rectangulo es:" + area);
+               
+        
+    }
+    
+}
